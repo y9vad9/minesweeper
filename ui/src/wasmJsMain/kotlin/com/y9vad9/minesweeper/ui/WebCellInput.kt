9f858ee3fe664f69b0ab15c0b0f1a6f77f19c689
Vadym Yaroshchuk <contact@y9vad9.com>
2026-05-26 23:07:20 +0200

    fix(web): click on game board on mobile is not recognized
    Signed-off-by: Vadym Yaroshchuk <contact@y9vad9.com>

--- ui/src/wasmJsMain/kotlin/com/y9vad9/minesweeper/ui/WebCellInput.kt
+++ ui/src/wasmJsMain/kotlin/com/y9vad9/minesweeper/ui/WebCellInput.kt
@@ -4,7 +4,13 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerButton
 import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.PointerType
 import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import kotlinx.coroutines.withTimeoutOrNull
 
 @OptIn(ExperimentalComposeUiApi::class)
 object WebCellInput : CellInputModifier {
@@ -18,11 +24,25 @@ object WebCellInput : CellInputModifier {
         .onPointerEvent(PointerEventType.Enter) { onHoverChange(true) }
         .onPointerEvent(PointerEventType.Exit) { onHoverChange(false) }
         .onPointerEvent(PointerEventType.Release) { event ->
+            if (event.changes.firstOrNull()?.type == PointerType.Touch) return@onPointerEvent
             when (event.button) {
                 PointerButton.Primary -> onReveal()
                 PointerButton.Secondary -> onFlag()
                 PointerButton.Tertiary -> onChord()
                 else -> Unit
+            }
+        }
+        .pointerInput(onReveal, onFlag) {
+            val longPressTimeout = viewConfiguration.longPressTimeoutMillis
+            awaitEachGesture {
+                val down = awaitFirstDown()
+                if (down.type != PointerType.Touch) return@awaitEachGesture
+                val up = withTimeoutOrNull(longPressTimeout) { waitForUpOrCancellation() }
+                if (up == null) {
+                    onFlag()
+                } else {
+                    onReveal()
+                }
             }
         }
 }
